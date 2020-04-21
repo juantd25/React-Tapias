24689fd034ba8db17fb8d0f78d277ac7039bc287
Juan Esteban Tapias Duque <juan.tapias@flagsoluciones.com>
2020-04-20 21:29:52 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const app = props => {
+	const [personsState, setPersonsState] = useState({
+		persons: [
+			{name: 'pedro', age: 20},
+			{name: 'ramiro', age: 21}
+		]
+	});
 
-export default App;
+	const [otherState, setOtherState] = useState('');
+
+	console.log(personsState, otherState);
+
+	const switchNameHandler = () => {
+		// console.log('Was Clicked!');
+		setPersonsState({
+			persons: [
+				{name: 'ana', age: 24},
+				{name: 'liza', age: 23}
+			]
+		});
+		setOtherState('sin cambiar');
+	};
+
+	const oldswitchNameHandler = () => {
+		// console.log('Was Clicked!');
+		setPersonsState({
+			persons: [
+				{name: 'pedro', age: 20},
+				{name: 'ramiro', age: 21}
+			]
+		});
+
+		setOtherState('ha cambiado');
+	};
+
+	return (
+		<div className="App">
+			<h1>Murillo manko</h1>
+			<button onClick={switchNameHandler}>Switch Name</button>
+			<button onClick={oldswitchNameHandler}>Old Switch Name</button>
+			<Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+			<Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
+				<Person name="children" age="13" />
+			</Person>
+		</div>
+	);
+};
+
+export default app;
