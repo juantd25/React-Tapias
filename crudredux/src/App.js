36b8666c9df5ec87702634139a81d13dcaf91c8d
juantd25 <juantd25@hotmail.com>
2021-05-06 21:02:51 -0500

    CRUD Redux

---
+++ crudredux/src/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Productos from './components/Productos';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Productos} />
+          <Route exact path="/productos/nuevo" component={NuevoProducto} />
+          <Route exact path="/productos/editar/:id" component={EditarProducto} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
